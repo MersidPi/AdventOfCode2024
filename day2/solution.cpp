a53456bf770564813a8e0cc38a19870716356f56
MersidPi <mpilipovic1@etf.unsa.ba>
2024-12-02 10:05:18 +0100

    day2 some cleanup to make it nice

--- day2/solution.cpp
+++ day2/solution.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include <sstream>
-#include <algorithm>
-#include <unordered_map>
 
 std::vector<std::string> extractLinesFromInputFile (std::string fileName) {
     std::ifstream inputFile(fileName);
@@ -15,146 +12,65 @@ std::vector<std::string> extractLinesFromInputFile (std::string fileName) {
     return lines;
 }
 
-bool isAsc(std::vector<int> v) {
-    std::vector<int> diff;
-    int sum = 0;
-    for (int i = 0; i < v.size() - 1; i++) {
-        diff.push_back(v[i] - v[i + 1]);
-        if (v[i + 1] - v[i] > 0)
-            sum += 1;
-        else sum--;
+bool isSafe(std::vector<int> v) {
+    bool asc = v[0] < v[1];
+    int counter = 0;
+    for (int j = 0; j < v.size() - 1; j++) {
+        if (!asc && (v[j] - v[j + 1] == 1 || 
+                    v[j] - v[j + 1] == 2 || 
+                    v[j] - v[j + 1] == 3)) {
+            continue;
+        } else if (asc && (v[j] - v[j + 1] == -1 || 
+                    v[j] - v[j + 1] == -2 || 
+                    v[j] - v[j + 1] == -3)) {
+            continue;
+        } 
+        return false;
     }
-    return sum > 0;
+    return true;
 }
 
-
-int part1 (std::vector<std::string> linesFromInputFile) {
+int part1 (const std::vector<std::string>& linesFromInputFile) {
     int sum = 0;
-    std::vector<std::vector<int>> vec;
+    std::vector<int> vec;
     for (std::string line : linesFromInputFile) {
         std::istringstream lineStream(line);
-        int num = -1;
-        lineStream >> num;
-        if (num != -1) {
-            vec.push_back(std::vector<int>());
-            while (lineStream.peek() == ' ') {
-                vec.at(vec.size() - 1).push_back(num);
-                lineStream >> num;
-            }
-            vec.at(vec.size() - 1).push_back(num);
+        int num;
+        while (!lineStream.eof()) {
+            lineStream >> num;
+            vec.push_back(num);
         }
-    }
-    // for (auto x : vec) {
-    //     for (auto s : x) std::cout << s << ' ';
-    //     std::cout << '\n';
-    // }
-
-    auto vec2 = vec;
-    std::vector<int> unsafe, SAFE;
-
-    for (int i = 0; i < vec.size(); i++) {
-        bool safe = true;
-        bool asc = vec.at(i).at(0) < vec.at(i)[1];
-        int counter = 0;
-        for (int j = 0; j < vec.at(i).size() - 1; j++) {
-            if (!asc && (vec.at(i).at(j) - vec.at(i)[j + 1] == 1 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == 2 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == 3)) {
-                continue;
-            } else if (asc && (vec.at(i).at(j) - vec.at(i)[j + 1] == -1 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == -2 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == -3)) {
-                continue;
-            } 
-            safe = false;
-            break;
-        }
-
-
-
-        
-
-        if (safe) {
+        if (isSafe(vec))
             sum++;
-            // std::cout << i << '\n';
-            }
+        vec.resize(0);
     }
     return sum;
 }
 
 int part2 (const std::vector<std::string>& linesFromInputFile) {
     int sum = 0;
-    std::vector<std::vector<int>> vec;
+    std::vector<int> vec;
     for (std::string line : linesFromInputFile) {
         std::istringstream lineStream(line);
-        int num = -1;
-        lineStream >> num;
-        if (num != -1) {
-            vec.push_back(std::vector<int>());
-            while (lineStream.peek() == ' ') {
-                vec.at(vec.size() - 1).push_back(num);
-                lineStream >> num;
-            }
-            vec.at(vec.size() - 1).push_back(num);
+        int num;
+        while (!lineStream.eof()) {
+            lineStream >> num;
+            vec.push_back(num);
         }
-    }
-    /*for (auto x : vec) {
-        for (auto s : x) std::cout << s << ' ';
-        std::cout << '\n';
-    }*/
-
-    auto vec2 = vec;
-    std::vector<int> unsafe, SAFE;
-
-    for (int i = 0; i < vec.size(); i++) {
-        bool safe = true;
-        bool asc = vec.at(i).at(0) < vec.at(i)[1];
-        int counter = 0;
-        for (int j = 0; j < vec.at(i).size() - 1; j++) {
-            if (!asc && (vec.at(i).at(j) - vec.at(i)[j + 1] == 1 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == 2 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == 3)) {
-                continue;
-            } else if (asc && (vec.at(i).at(j) - vec.at(i)[j + 1] == -1 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == -2 || 
-                        vec.at(i).at(j) - vec.at(i)[j + 1] == -3)) {
-                continue;
-            } 
-            safe = false;
-            break;
-        }
-
-        if (!safe) {
-            // std::cout << i << " uklanjamo '\n";
-            for (int k = 0; k < vec.at(i).size(); k++) {
-                auto temp = vec.at(i);
-                temp.erase(temp.begin() + k);
-                safe = true;
-                bool asc = temp[0] < temp[1];
-                int counter = 0;
-                for (int j = 0; j < temp.size() - 1; j++) {
-                    if (!asc && (temp.at(j) - temp[j + 1] == 1 || 
-                                temp.at(j) - temp[j + 1] == 2 || 
-                                temp.at(j) - temp[j + 1] == 3)) {
-                        continue;
-                    } else if (asc && (temp.at(j) - temp[j + 1] == -1 || 
-                                temp.at(j) - temp[j + 1] == -2 || 
-                                temp.at(j) - temp[j + 1] == -3)) {
-                        continue;
-                    } 
-                    safe = false;
+        if (isSafe(vec))
+            sum++;
+        else {
+            auto temp = vec;
+            for (int i = 0; i < vec.size(); i++) {
+                temp = vec;
+                temp.erase(temp.begin() + i);
+                if (isSafe(temp)) {
+                    sum++;
                     break;
                 }
-                if (safe) {
-                    // std::cout<<"ispravljeni je ok\n";
-                    break;}
             }
         }
-
-        
-
-        if (safe) 
-            sum++;
+        vec.resize(0);
     }
     return sum;
 }
@@ -163,6 +79,5 @@ int main () {
     std::vector<std::string> linesFromInputFile = extractLinesFromInputFile("input.txt");
     int part1result = part1(linesFromInputFile);std::cout << "\nPart 1: " << part1result << "\n";
     int part2result = part2(linesFromInputFile);std::cout << "Part 2: " << part2result << "\n\n";
-    std::cout<<isAsc({3,2,0,-1,-2});
     return 0;
 }
