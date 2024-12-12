78c05a4099184ee10dfb099a065b2eb6a1ecc895
MersidPi <mpilipovic1@etf.unsa.ba>
2024-12-12 10:48:18 +0100

    day12 cleanup

--- day12/solution.cpp
+++ day12/solution.cpp
@@ -16,250 +16,132 @@ std::vector<std::string> extractLinesFromInputFile (std::string fileName) {
     return lines;
 }
 
-std::map<std::pair<ull,ull>, ull> coordsMap, edg;
-ull edges = 0;
+std::vector<std::string> putBoundaries (std::vector<std::string> matrix, char c) {
+    ull sum = 0, M = matrix.size(), N = matrix[0].size();
+    std::vector<std::string> result(M+2, std::string(N+2, ' '));
+    for (ull i = 0; i < M+2; i++) {
+        for (ull j = 0; j < N+2; j++)
+            if (i == 0 || i == M+1 || j == 0 || j == N+1)
+                result[i][j] = c;
+            else
+                result[i][j] = matrix[i-1][j-1];
+    }
+    return result;
+}
+
+std::map<std::pair<ull,ull>, ull> coordsInnerPoints, coordsEdges;
+ull numOfEdges = 0;
 
 void calculate (const std::vector<std::string> &m, ull i, ull j) {
     char c = m[i][j];
-    if (coordsMap.count({i,j}) == 1) {
+    if (coordsInnerPoints.count({i,j}) == 1) {
         return;
     }
-    coordsMap[{i, j}]++;
+    coordsInnerPoints[{i, j}]++;
     if (m[i][j + 1] == c) {
         calculate(m, i, j + 1);
     }
     else {
-        edges++;
-        edg[{i, j + 1}]++;
+        numOfEdges++;
+        coordsEdges[{i, j + 1}]++;
     }
     if (m[i][j - 1] == c) {
         calculate(m, i, j - 1);
     }
     else {
-        edges++;
-        edg[{i, j - 1}]++;
+        numOfEdges++;
+        coordsEdges[{i, j - 1}]++;
     }
     if (m[i + 1][j] == c) {
         calculate(m, i + 1, j);
     }
     else {
-        edges++;
-        edg[{i + 1, j}]++;
+        numOfEdges++;
+        coordsEdges[{i + 1, j}]++;
     }
     if (m[i - 1][j] == c) {
         calculate(m, i - 1, j);
     }
     else {
-        edges++;
-        edg[{i - 1, j}]++;
+        numOfEdges++;
+        coordsEdges[{i - 1, j}]++;
     }
-    
 }
 
 ull part1 (std::vector<std::string> m) {
-    ull sum = 0, M = m.size(), N = m[0].size();
-    std::vector<std::string> map(M+2, std::string(N+2, ' '));
-    for (ull i = 0; i < M+2; i++) {
-        for (ull j = 0; j < N+2; j++)
-            if (i == 0 || i == M+1 || j == 0 || j == N+1)
-                map[i][j] = '.';
-            else
-                map[i][j] = m[i-1][j-1];
-    }
-    for (ull i = 0; i < M+2; i++) {
-        for (ull j = 0; j < N+2; j++) {
+    std::vector<std::string> map = putBoundaries(m, '.');
+    ull sum = 0, M = map.size(), N = map[0].size();
+    for (ull i = 0; i < M; i++) {
+        for (ull j = 0; j < N; j++) {
             if (map[i][j] == '.')
                 continue;
-            edges = 0;
+            numOfEdges = 0;
             calculate(map, i, j);
-            sum += edges * coordsMap.size();
-            for (auto [key, val] : coordsMap)
+            sum += numOfEdges * coordsInnerPoints.size();
+            for (auto [key, val] : coordsInnerPoints)
                 map[key.first][key.second] = '.';
-            coordsMap.clear();
-            edg.clear();
+            coordsInnerPoints.clear();
+            coordsEdges.clear();
         }
     }
     return sum;
 }
 
 ull part2 (std::vector<std::string> m) {
-    ull sum = 0, M = m.size(), N = m[0].size();
-    std::vector<std::string> map(M+2, std::string(N+2, ' '));
-    // std::multimap<char, std::set<ull, ull>> multimap;
-    for (ull i = 0; i < M+2; i++) {
-        for (ull j = 0; j < N+2; j++)
-            if (i == 0 || i == M+1 || j == 0 || j == N+1)
-                map[i][j] = '.';
-            else
-                map[i][j] = m[i-1][j-1];
-    }
-    std::ofstream fs("output1.txt");
-    for (auto x : map) {
-        for (auto y : x){
-            fs << y ;
-        }
-        fs << '\n';
-    }
-
-    for (ull i = 0; i < M+2; i++) {
-        for (ull j = 0; j < N+2; j++) {
+    std::vector<std::string> map = putBoundaries(m, '.');
+    ull sum = 0, M = map.size(), N = map[0].size();
+    for (ull i = 0; i < M; i++) {
+        for (ull j = 0; j < N; j++) {
             if (map[i][j] == '.')
                 continue;
-            edges = 0;
             calculate(map, i, j);
-            for (auto [key, val] : coordsMap)
+            for (auto [key, val] : coordsInnerPoints)
                 map[key.first][key.second] = '#';
-            
-            ull k = i, l = j;
-            // do {
-            //     std::cout << k << ' ' << l << '\n';
-            //     while (edg.count({k + 1, l}) == 1 /*map[k + 1][l] == '#'*/) {
-            //         edges--;
-            //         k++;
-            //     }
-            //     if (k == i && l == j)
-            //         break;
-            //     while (edg.count({k, l + 1}) == 1 /*map[k][l + 1] == '#'*/) {
-            //         edges--;
-            //         l++;
-            //     }
-            //     if (k == i && l == j)
-            //         break;
-            //     while (edg.count({k - 1, l}) == 1 /*map[k - 1][l] == '#'*/) {
-            //         edges--;
-            //         k--;
-            //     }
-            //     if (k == i && l == j)
-            //         break;
-            //     while (edg.count({k, l - 1}) == 1 /*map[k][l - 1] == '#'*/) {
-            //         edges--;
-            //         l--;
-            //     }
-            //     if (k == i && l == j)
-            //         break;
-            // }
-            // while (!(k == i && l == j));
             auto mapCopy = map;
-            for (auto [key, val] : edg) {
-                // std::cout << key.first << ' ' << key.second << '\n';
+            for (auto [key, val] : coordsEdges)
                 mapCopy[key.first][key.second] = '$';
-            }
-
-            // fs << " prva mapcopy \n";
-            // for (auto x : mapCopy) {
-            //     for (auto y : x){
-            //         fs << y ;
-            //     }
-            //     fs << '\n';
-            // }
-            // fs << '\n';
-
-            auto mapPrull = mapCopy;
-            edges = 0;
-            ull tmp = 0;
-                for (ull k = 0; k < M+2; k++) {
-                    for (ull l = 0; l < N+2; l++) {
-                        if (mapCopy[k][l] == '#') {
-                            if (mapCopy[k+1][l+0] == '$') {
-                                edges++;
-                                tmp++;
-                                while (mapCopy[k][l] == '#' && mapCopy[k+1][l+0] == '$') {
-                                    l++;
-                                }
-                            }
-                        }
+            numOfEdges = 0;
+            for (ull k = 0; k < M; k++)
+                for (ull l = 0; l < N; l++)
+                    if (mapCopy[k][l] == '#' && mapCopy[k+1][l+0] == '$') {
+                        numOfEdges++;
+                        while (mapCopy[k][l] == '#' && mapCopy[k+1][l+0] == '$')
+                            l++;
                     }
-                }
-                fs << " DONJA "<< tmp << '\n';
-                tmp = 0;
-                for (ull k = 0; k < M+2; k++) {
-                    for (ull l = 0; l < N+2; l++) {
-                        if (mapCopy[k][l] == '#') {
-                            if (mapCopy[k-1][l+0] == '$') {
-                                tmp++;
-                                edges++;
-                                while (mapCopy[k][l] == '#' && mapCopy[k-1][l+0] == '$') {
-                                    l++;
-                                }
-                            }
-                        }
+            for (ull k = 0; k < M; k++)
+                for (ull l = 0; l < N; l++)
+                    if (mapCopy[k][l] == '#' && mapCopy[k-1][l+0] == '$') {
+                        numOfEdges++;
+                        while (mapCopy[k][l] == '#' && mapCopy[k-1][l+0] == '$')
+                            l++;
                     }
-                }
-                fs << " GORNJA "<< tmp << '\n';
-                tmp = 0;
-
-                for (ull l = 0; l < N+2; l++) {
-                    for (ull k = 0; k < M+2; k++) {
-                        if (mapCopy[k][l] == '#') {
-                            fs << " usao za " << k << " " << l << '\n';
-                            if (mapCopy[k + 0][l + 1] == '$') {
-                                mapPrull[k + 0][l + 1] = '0';
-                                tmp++;
-                                edges++;
-                                while (mapCopy[k][l] == '#' && mapCopy[k + 0][l + 1] == '$') {
-                                mapPrull[k + 0][l + 1] = '0';
-                                    k++;
-                                }
-                            }
-                        }
+            for (ull l = 0; l < N; l++)
+                for (ull k = 0; k < M; k++)
+                    if (mapCopy[k][l] == '#' && mapCopy[k + 0][l + 1] == '$') {
+                        numOfEdges++;
+                        while (mapCopy[k][l] == '#' && mapCopy[k + 0][l + 1] == '$')
+                            k++;
                     }
-                }
-                fs << " DESNA "<< tmp << '\n';
-                tmp = 0;
-
-                for (ull l = 0; l < N+2; l++) {
-                    for (ull k = 0; k < M+2; k++) {
-                        if (mapCopy[k][l] == '#') {
-                            if (mapCopy[k][l] == '#' && mapCopy[k + 0][l - 1] == '$') {
-
-                                tmp++;
-                                edges++;
-                                while (mapCopy[k][l] == '#' &&  mapCopy[k + 0][l - 1] == '$') {
-                                    k++;
-                                }
-                            }
-                        }
+            for (ull l = 0; l < N; l++)
+                for (ull k = 0; k < M; k++)
+                    if (mapCopy[k][l] == '#' && mapCopy[k + 0][l - 1] == '$') {
+                        numOfEdges++;
+                        while (mapCopy[k][l] == '#' &&  mapCopy[k + 0][l - 1] == '$')
+                            k++;
                     }
-                }
-                fs << " LIJEVA "<< tmp << '\n';
-                tmp = 0;
-
-            fs << "finalno " << edges << '\n';
-            for (auto x : mapPrull) {
-                for (auto y : x){
-                    fs << y ;
-                }
-                fs << '\n';
-            }
-            
-            sum += edges * coordsMap.size();
-            fs << "dodajemo " << edges * coordsMap.size() << '\n';
-            for (auto [key, val] : coordsMap)
+            sum += numOfEdges * coordsInnerPoints.size();
+            for (auto [key, val] : coordsInnerPoints)
                 map[key.first][key.second] = '.';
-            coordsMap.clear();
-            edg.clear();
+            coordsInnerPoints.clear();
+            coordsEdges.clear();
         }
     }
     return sum;
 }
-// 851300 TOO LOW
-
-
 
 int main () {
-    std::vector<std::string> m = extractLinesFromInputFile("input.txt");
-
-    ull sum = 0, M = m.size(), N = m[0].size();
-    std::vector<std::string> map(M+2, std::string(N+2, ' '));
-    // std::multimap<char, std::set<ull, ull>> multimap;
-    for (ull i = 0; i < M+2; i++) {
-        for (ull j = 0; j < N+2; j++)
-            if (i == 0 || i == M+1 || j == 0 || j == N+1)
-                map[i][j] = '.';
-            else
-                map[i][j] = m[i-1][j-1];
-    }
-    auto linesFromInputFile = map;
+    std::vector<std::string> linesFromInputFile = extractLinesFromInputFile("input.txt");
+    linesFromInputFile = putBoundaries(linesFromInputFile, '.');
     ull part1result = part1(linesFromInputFile); std::cout << "\nPart 1: " << part1result << "\n";
     ull part2result = part2(linesFromInputFile); std::cout << "Part 2: " << part2result << "\n\n";
     return 0;
